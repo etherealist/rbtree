da00f54870a0f91548d51d7ca9270bd8a461e987
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2021-10-06 01:27:05 +0200

    add latest files as-is

---
+++ example.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2019 Jean-Louis Fuchs <ganwell@fangorn.ch>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#ifndef rb_example_h
+#define rb_example_h
+
+#include "rbtree.h"
+
+struct book_s;
+typedef struct book_s book_t;
+struct book_s {
+    char    isbn[14];
+    char*   title;
+    char*   author;
+    char    color;
+    book_t* parent;
+    book_t* left;
+    book_t* right;
+};
+
+#define bk_cmp_m(x, y) memcmp(x->isbn, y->isbn, 13)
+rb_bind_decl_m(bk, book_t)
+
+#endif
